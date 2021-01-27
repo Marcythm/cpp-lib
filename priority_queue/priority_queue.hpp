60b85fb3bcd8b783975e96a804f97ee4a2e09f99
Marcythm <marcythm@gmail.com>
2021-01-27 18:11:30 +0800

    impl: priority_queue

--- priority_queue/priority_queue.hpp
+++ priority_queue/priority_queue.hpp
@@ -4,68 +4,111 @@
 #include <cstddef>
 #include <functional>
 #include "exceptions.hpp"
+#include "utility.hpp"
 
 namespace sjtu {
 
-/**
- * a container like std::priority_queue which is a heap internal.
- */
-template<typename T, class Compare = std::less<T>>
+template <typename T, class Compare = std::less<T>>
 class priority_queue {
 public:
-	/**
-	 * TODO constructors
-	 */
-	priority_queue() {}
-	priority_queue(const priority_queue &other) {}
-	/**
-	 * TODO deconstructor
-	 */
-	~priority_queue() {}
-	/**
-	 * TODO Assignment operator
-	 */
-	priority_queue &operator=(const priority_queue &other) {}
-	/**
-	 * get the top of the queue.
-	 * @return a reference of the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	const T & top() const {
+	using value_type		=	T;
+	using reference			=	value_type&;
+	using const_reference	=	const value_type&;
+	using size_type			=	size_t;
 
-	}
-	/**
-	 * TODO
-	 * push new element to the priority queue.
-	 */
-	void push(const T &e) {
+private:
 
+	struct HeapNode {
+		size_type depth;
+		HeapNode *ls, *rs;
+		value_type value;
+
+		HeapNode(const HeapNode &rhs) = default;
+		HeapNode(HeapNode &&rhs): depth(rhs.depth), ls(rhs.ls), rs(rhs.rs), value(std::move(rhs.value)) {
+			rhs.ls = rhs.rs = nullptr;
+		}
+
+		HeapNode(const value_type &_value = value_type())
+			: depth(0), ls(nullptr), rs(nullptr), value(_value) {}
+		HeapNode(size_type _depth, HeapNode *_ls, HeapNode *_rs, const value_type &_value)
+			: depth(_depth), ls(_ls), rs(_rs), value(_value) { self_adjust(); }
+
+		void self_adjust() {
+			if (ls == nullptr or (rs != nullptr and ls->depth < rs->depth))
+				std::swap(ls, rs);
+			depth = (rs != nullptr ? rs->depth + 1 : 0);
+		}
+
+		~HeapNode() = default;
+	};
+
+	HeapNode* clone(HeapNode *u) {
+		return u != nullptr ? new HeapNode(u->depth, clone(u->ls), clone(u->rs), u->value) : nullptr;
 	}
-	/**
-	 * TODO
-	 * delete the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
+	void remove(HeapNode *u) {
+		if (u != nullptr) {
+			remove(u->ls);
+			remove(u->rs);
+			delete u;
+		}
+	}
+	HeapNode* merge(HeapNode *lhs, HeapNode *rhs) {
+		if (lhs == nullptr) return rhs;
+		if (rhs == nullptr) return lhs;
+		if (__comp(lhs->value, rhs->value)) std::swap(lhs, rhs);
+		lhs->rs = merge(lhs->rs, rhs);
+		lhs->self_adjust();
+		return lhs;
+	}
+
+	HeapNode *__root;
+	size_type __size;
+	Compare __comp;
+
+public:
+	priority_queue(): __root(nullptr), __size(0), __comp(Compare()) {}
+	priority_queue(const priority_queue &other)
+		: __root(clone(rhs.__root)), __size(rhs.__size), __comp(other.__comp) {}
+
+	~priority_queue() { remove(__root); }
+
+	priority_queue &operator=(const priority_queue &other) {
+		if (this == &other) return *this;
+		remove(__root);
+		__root = clone(other.__root);
+		__size = other.__size;
+		return *this;
+	}
+
+	const_reference top() const {
+		if (__root == nullptr)
+			throw container_is_empty();
+		return __root->value;
+	}
+
+	void push(const value_type &value) {
+		__root = merge(__root, new Node(value));
+		++__size;
+	}
+
 	void pop() {
-
+		if (__root == nullptr)
+			throw container_is_empty();
+		auto tmp = __root;
+		__root = merge(__root->ls, __root->rs);
+		delete tmp;
+		--__size;
 	}
-	/**
-	 * return the number of the elements.
-	 */
-	size_t size() const {
 
-	}
-	/**
-	 * check if the container has at least an element.
-	 * @return true if it is empty, false if it has at least an element.
-	 */
-	bool empty() const {
+	size_type size() const { return __size; }
 
-	}
-	/**
-	 * return a merged priority_queue with at least O(logn) complexity.
-	 */
+	bool empty() const { return root == nullptr; }
+
 	void merge(priority_queue &other) {
+		__root = merge(__root, other.__root);
+		__size += other.__size;
+		other.__root = nullptr;
+		other.__size = 0;
 	}
 };
 
