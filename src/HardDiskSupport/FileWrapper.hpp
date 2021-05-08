0ce453c1620f22eb125e72b785589aa548444782
Marcythm <marcythm@gmail.com>
2021-05-08 17:23:18 +0800

    update indent

--- src/HardDiskSupport/FileWrapper.hpp
+++ src/HardDiskSupport/FileWrapper.hpp
@@ -12,119 +12,127 @@ namespace HardDisk {
 
 #ifdef C_STYLE_HardDiskIO
 
-	struct FileWrapper {
-		using Self			= FileWrapper;
-		using offset_type	= i64;
+    struct FileWrapper {
+        using Self          = FileWrapper;
+        using offset_type   = i64;
 
-		std::FILE *file;
+        std::FILE *file;
 
-		FileWrapper(): file(nullptr) { }
-		explicit FileWrapper(const char *filename) { open(filename); }
-		explicit FileWrapper(const std::string &filename) { open(filename.c_str()); }
+        FileWrapper(): file(nullptr) { }
+        explicit FileWrapper(const char *filename) { open(filename); }
+        explicit FileWrapper(const std::string &filename) { open(filename.c_str()); }
 
-		FileWrapper(Self &&other): file(other.file) { other.file = nullptr; }
-		FileWrapper(const Self &) = delete;
+        FileWrapper(Self &&other): file(other.file) { other.file = nullptr; }
+        FileWrapper(const Self &) = delete;
 
-		~FileWrapper() { if (file != nullptr) std::fclose(file); }
+        ~FileWrapper() { if (file != nullptr) std::fclose(file); }
 
-		auto open(const char *filename) -> bool {
-			if (file = std::fopen(filename, "r+b"); file == nullptr)
-				return file = std::fopen(filename, "w+b"), false;
-			return true;
-		}
-		auto open(const std::string &filename) -> bool { return open(filename.c_str()); }
+        auto open(const char *filename) -> bool {
+            if (file = std::fopen(filename, "r+b"); file == nullptr)
+                return file = std::fopen(filename, "w+b"), false;
+            return true;
+        }
+        auto open(const std::string &filename) -> bool { return open(filename.c_str()); }
 
-		auto is_open() const -> bool { return file != nullptr; }
-		auto close() -> void { std::fclose(file); }
-		auto flush() -> void { std::fflush(file); }
+        auto is_open() const -> bool { return file != nullptr; }
+        auto close() -> void { std::fclose(file); }
+        auto flush() -> void { std::fflush(file); }
 
-		auto tell() const -> offset_type { return std::ftell(file); }
-		auto seek(offset_type offset) -> void { std::fseek(file, offset, SEEK_SET); }
-		auto seek_end() -> void { std::fseek(file, 0, SEEK_END); }
+        auto tell() const -> offset_type { return std::ftell(file); }
+        auto seek(offset_type offset) -> void {
+            if (offset == -1)
+                std::fseek(file, 0, SEEK_END);
+            else
+                std::fseek(file, offset, SEEK_SET);
+        }
 
-		template <typename T>
-		auto read() -> T {
-			T value;
-			std::fread(std::addressof(value), sizeof(T), 1, file);
-			return value;
-		}
+        template <typename T>
+        auto read() -> T {
+            T value;
+            std::fread(std::addressof(value), sizeof(T), 1, file);
+            return value;
+        }
 
-		template <typename T>
-		auto read(T &obj) -> void {
-			std::fread(std::addressof(obj), sizeof(T), 1, file);
-		}
+        template <typename T>
+        auto read(T &obj) -> void {
+            std::fread(std::addressof(obj), sizeof(T), 1, file);
+        }
 
-		template <typename T>
-		auto write(const T &obj) -> void {
-			std::fwrite(std::addressof(obj), sizeof(T), 1, file);
-		}
+        template <typename T>
+        auto write(const T &obj) -> void {
+            std::fwrite(std::addressof(obj), sizeof(T), 1, file);
+        }
 
-		template <typename T>
-		auto append(const T &obj) -> offset_type {
-			seek_end();
-			offset_type offset = tell();
-			write(obj);
-			return offset;
-		}
-	};
+        template <typename T>
+        auto append(const T &obj) -> offset_type {
+            seek(-1);
+            offset_type offset = tell();
+            write(obj);
+            return offset;
+        }
+    };
 
 #else
 
-	struct FileWrapper {
-		using Self			= FileWrapper;
-		using offset_type	= u32;
+    struct FileWrapper {
+        using Self          = FileWrapper;
+        using offset_type   = u32;
 
-		std::fstream file;
+        std::fstream file;
 
-		FileWrapper(): file() { }
-		explicit FileWrapper(const char *filename) { open(filename); }
-		explicit FileWrapper(const std::string &filename) { open(filename.c_str()); }
+        FileWrapper(): file() { }
+        explicit FileWrapper(const char *filename) { open(filename); }
+        explicit FileWrapper(const std::string &filename) { open(filename.c_str()); }
 
-		FileWrapper(Self &&other): file(std::move(other.file)) { }
-		FileWrapper(const Self &) = delete;
+        FileWrapper(Self &&other): file(std::move(other.file)) { }
+        FileWrapper(const Self &) = delete;
 
-		~FileWrapper() { if (file.is_open()) file.close(); }
+        ~FileWrapper() { if (file.is_open()) file.close(); }
 
-		auto open(const char *filename) -> bool {
-			if (file.open(filename, std::ios::in | std::ios::out | std::ios::binary); file.is_open())
-				return true;
-			return file.open(filename, "w+b"), false;
-		}
-		auto open(const std::string &filename) -> bool { return open(filename.c_str()); }
+        auto open(const char *filename) -> bool {
+            if (file.open(filename, std::ios::in | std::ios::out | std::ios::binary); file.is_open())
+                return true;
+            return file.open(filename, "w+b"), false;
+        }
+        auto open(const std::string &filename) -> bool { return open(filename.c_str()); }
 
-		auto is_open() const -> bool { return file.is_open(); }
-		auto close() -> void { file.close(); }
-		auto flush() -> void { file.flush(); }
+        auto is_open() const -> bool { return file.is_open(); }
+        auto close() -> void { file.close(); }
+        auto flush() -> void { file.flush(); }
 
-		auto tell() const -> offset_type { return file.tellg(); }
-		auto seek(offset_type offset) -> void { file.seekg(offset, std::fstream::beg); }
-		auto seek_end() -> void { file.seekg(0, std::fstream::end); }
+        auto tell() const -> offset_type { return file.tellg(); }
+        auto seek(offset_type offset) -> void {
+            if (offset == -1)
+                file.seekg(0, std::fstream::end);
+            else
+                file.seekg(offset, std::fstream::beg);
+        }
 
-		template <typename T>
-		auto read() -> T {
-			T value;
-			file.read(reinterpret_cast<char*>(std::addressof(value)), sizeof(T));
-			return value;
-		}
+        template <typename T>
+        auto read() -> T {
+            T value;
+            file.read(reinterpret_cast<char*>(std::addressof(value)), sizeof(T));
+            return value;
+        }
 
-		template <typename T>
-		auto read(T &obj) -> void {
-			file.read(reinterpret_cast<char*>(std::addressof(obj)), sizeof(T));
-		}
+        template <typename T>
+        auto read(T &obj) -> void {
+            file.read(reinterpret_cast<char*>(std::addressof(obj)), sizeof(T));
+        }
 
-		template <typename T>
-		auto write(const T &obj) -> void {
-			file.write(reinterpret_cast<char*>(std::addressof(obj)), sizeof(T));
-		}
+        template <typename T>
+        auto write(const T &obj) -> void {
+            file.write(reinterpret_cast<char*>(std::addressof(obj)), sizeof(T));
+        }
 
-		template <typename T>
-			auto append(const T &obj) -> offset_type {
-				seek_end();
-				offset_type offset = tell();
-				write(obj);
-				return offset;
-			}
-	};
+        template <typename T>
+        auto append(const T &obj) -> offset_type {
+            seek(-1);
+            offset_type offset = tell();
+            write(obj);
+            return offset;
+        }
+    };
 
 #endif
 
--- src/HardDiskSupport/Record.hpp
+++ src/HardDiskSupport/Record.hpp
@@ -9,96 +9,96 @@ using namespace __config;
 
 namespace HardDisk {
 
-	struct Record {
-		using Self			= Record;
-		using offset_type	= FileWrapper::offset_type;
+    struct Record {
+        using Self          = Record;
+        using offset_type   = FileWrapper::offset_type;
 
-		offset_type offset;
-
-
-		Record(): offset(static_cast<offset_type>(-1)) {}
-		Record(offset_type __offset): offset(__offset) {}
-		Record(Self &&other): offset(other.offset) { other.offset = static_cast<offset_type>(-1); }
-		Record(const Self &other): offset(other.offset) {}
-
-		auto operator = (Self &&rhs) -> Self& {
-			offset = rhs.offset;
-			rhs.offset = static_cast<offset_type>(-1);
-			return *this;
-		}
-		auto operator = (const Self &rhs) -> Self& { return offset = rhs.offset, *this; }
-
-		auto empty() const -> bool { return offset == static_cast<offset_type>(-1); }
-
-		template <typename T>
-		auto get(FileWrapper &io) const -> T {
-			if (empty()) throw "try to load from an empty record";
-			T value;
-			load(io, value);
-			return value;
-		}
-
-		template <typename T>
-		auto save(FileWrapper &io, const T &value) -> Self {
-			if (empty())
-				return Record(offset = io.append(value));
-			io.seek(offset);
-			io.write(value);
-			return *this;
-		}
-		template <typename T>
-		auto load(FileWrapper &io, T &value) const -> Self {
-			if (empty()) throw "try to load from an empty record";
-			io.seek(offset);
-			io.read(value);
-			return *this;
-		}
-	};
-
-	template <typename T>
-	struct RecordPool {
-		Vec<Record> recs;
-
-		RecordPool(): recs() {}
-
-		auto alloc() -> Record {
-			if (recs.empty()) return Record();
-			Record tmp = recs.back();
-			recs.pop_back();
-			return tmp;
-		}
-
-		auto dealloc(const Record &rec) -> void {
-			recs.push_back(rec);
-		}
-	};
-
-	// template <typename T>
-	// struct Record {
-	// 	using Self			= Record;
-	// 	using value_type	= T;
-	// 	using offset_type	= FileWrapper::offset_type;
-
-	// 	offset_type offset;
+        offset_type offset;
 
 
-	// 	Record(): offset(static_cast<offset_type>(-1)) {}
-	// 	Record(offset_type __offset): offset(__offset) {}
-	// 	Record(const Self &other): offset(other.offset) {}
+        Record(): offset(static_cast<offset_type>(-1)) {}
+        Record(offset_type __offset): offset(__offset) {}
+        Record(Self &&other): offset(other.offset) { other.offset = static_cast<offset_type>(-1); }
+        Record(const Self &other): offset(other.offset) {}
 
-	// 	auto operator = (const Self &rhs) -> Self& { return offset = rhs.offset, *this; }
+        auto operator = (Self &&rhs) -> Self& {
+            offset = rhs.offset;
+            rhs.offset = static_cast<offset_type>(-1);
+            return *this;
+        }
+        auto operator = (const Self &rhs) -> Self& { return offset = rhs.offset, *this; }
 
-	//	auto empty() const -> bool { return offset == static_cast<offset_type>(-1); }
+        auto empty() const -> bool { return offset == static_cast<offset_type>(-1); }
 
-	// 	auto save(FileWrapper &io, const value_type &value) const -> void {
-	// 		io.seek(offset);
-	// 		io.write(value);
-	// 	}
-	// 	auto load(FileWrapper &io, value_type &value) const -> void {
-	// 		io.seek(offset);
-	// 		io.read(value);
-	// 	}
-	// };
+        template <typename T>
+        auto get(FileWrapper &io) const -> T {
+            if (empty()) throw "try to load from an empty record";
+            T value;
+            load(io, value);
+            return value;
+        }
+
+        template <typename T>
+        auto save(FileWrapper &io, const T &value) -> Self {
+            if (empty())
+                return Record(offset = io.append(value));
+            io.seek(offset);
+            io.write(value);
+            return *this;
+        }
+        template <typename T>
+        auto load(FileWrapper &io, T &value) const -> Self {
+            if (empty()) throw "try to load from an empty record";
+            io.seek(offset);
+            io.read(value);
+            return *this;
+        }
+    };
+
+    template <typename T>
+    struct RecordPool {
+        Vec<Record> recs;
+
+        RecordPool(): recs() {}
+
+        auto alloc() -> Record {
+            if (recs.empty()) return Record();
+            Record tmp = recs.back();
+            recs.pop_back();
+            return tmp;
+        }
+
+        auto dealloc(const Record &rec) -> void {
+            recs.push_back(rec);
+        }
+    };
+
+    // template <typename T>
+    // struct Record {
+    // 	using Self			= Record;
+    // 	using value_type	= T;
+    // 	using offset_type	= FileWrapper::offset_type;
+
+    // 	offset_type offset;
+
+
+    // 	Record(): offset(static_cast<offset_type>(-1)) {}
+    // 	Record(offset_type __offset): offset(__offset) {}
+    // 	Record(const Self &other): offset(other.offset) {}
+
+    // 	auto operator = (const Self &rhs) -> Self& { return offset = rhs.offset, *this; }
+
+    //	auto empty() const -> bool { return offset == static_cast<offset_type>(-1); }
+
+    // 	auto save(FileWrapper &io, const value_type &value) const -> void {
+    // 		io.seek(offset);
+    // 		io.write(value);
+    // 	}
+    // 	auto load(FileWrapper &io, value_type &value) const -> void {
+    // 		io.seek(offset);
+    // 		io.read(value);
+    // 	}
+    // };
 
 }
 
